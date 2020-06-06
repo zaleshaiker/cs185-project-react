edce0b91a7c8c1b9c975d428904a7f8ded0625f2
Zain Aleshaiker <zain@ucsb.com>
2020-06-06 00:00:06 -0700

    Updates

---
+++ precache-manifest.c4193c76fb987b29509462def43524ef.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81006e9f220254b3dced20338637b17d",
+    "url": "/cs185-project-react/index.html"
+  },
+  {
+    "revision": "0d64c614d358392cafc2",
+    "url": "/cs185-project-react/static/css/2.ccab356c.chunk.css"
+  },
+  {
+    "revision": "e5e69d75712f529b9763",
+    "url": "/cs185-project-react/static/css/main.cf4a627b.chunk.css"
+  },
+  {
+    "revision": "0d64c614d358392cafc2",
+    "url": "/cs185-project-react/static/js/2.a6a2c1b5.chunk.js"
+  },
+  {
+    "revision": "2a19b12333a6b135f09b424bfc4741b1",
+    "url": "/cs185-project-react/static/js/2.a6a2c1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e69d75712f529b9763",
+    "url": "/cs185-project-react/static/js/main.7885edb7.chunk.js"
+  },
+  {
+    "revision": "a054c171748d892aab64",
+    "url": "/cs185-project-react/static/js/runtime-main.9c810152.js"
+  },
+  {
+    "revision": "c6d6bab07ed6749fb96431eac12bbe29",
+    "url": "/cs185-project-react/static/media/ABeeZee-Regular.c6d6bab0.ttf"
+  },
+  {
+    "revision": "2d354e7e780646a9a3430905131a716f",
+    "url": "/cs185-project-react/static/media/back-to-top.2d354e7e.png"
+  },
+  {
+    "revision": "d67f0015db1a87aa096298fc5f8e8299",
+    "url": "/cs185-project-react/static/media/beach.d67f0015.jpg"
+  },
+  {
+    "revision": "2c60a770a3fd7f9aba6d4b3318fe7b23",
+    "url": "/cs185-project-react/static/media/dodge-logo.2c60a770.png"
+  },
+  {
+    "revision": "ef0d6a0069c4eb8c9e362f0318af1f82",
+    "url": "/cs185-project-react/static/media/img-1.ef0d6a00.jpg"
+  },
+  {
+    "revision": "8cdd14422cbb938326ccbf739a9558e5",
+    "url": "/cs185-project-react/static/media/img-2.8cdd1442.jpg"
+  },
+  {
+    "revision": "8a8ab93e78c0bc1e2a19469de4968c93",
+    "url": "/cs185-project-react/static/media/img-3.8a8ab93e.jpg"
+  },
+  {
+    "revision": "02614206773b90c0c0956a85a13483af",
+    "url": "/cs185-project-react/static/media/img-4.02614206.jpg"
+  },
+  {
+    "revision": "8100a0f9e9758d9767ab53a2db8fc328",
+    "url": "/cs185-project-react/static/media/img-5.8100a0f9.jpg"
+  },
+  {
+    "revision": "f94042097e6d6985e54bd25fdc7cb012",
+    "url": "/cs185-project-react/static/media/img-6.f9404209.jpg"
+  },
+  {
+    "revision": "883a1c87441f80a11895cee131f7c324",
+    "url": "/cs185-project-react/static/media/planets-logo.883a1c87.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cs185-project-react/precache-manifest.b6c6d28d91b636b8070ee29e6b67c964.js"
+  "/cs185-project-react/precache-manifest.c4193c76fb987b29509462def43524ef.js"
 );
 
 self.addEventListener('message', (event) => {
